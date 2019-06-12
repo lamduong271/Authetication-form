a82024c26ab033c844e65910ffbc7a53bd1ed701
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-06-12 23:47:04 +0300

    complete

--- authen/models.py
+++ authen/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +8,10 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MyUser(AbstractUser):
+    age = models.IntegerField(default=0)
+    gender_choice = ((0,'Female'),(1,'Male'))
+    gender = models.IntegerField(choices=gender_choice, default=0)
+    address = models.CharField(default='', max_length=255)
